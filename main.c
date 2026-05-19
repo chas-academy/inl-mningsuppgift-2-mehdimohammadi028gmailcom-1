9ab2358143dc9c72a516038895ef024c7e17fa4f
mehdimohammadi028gmailcom <99673774+mehdimohammadi028gmailcom@users.noreply.github.com>
2026-05-19 23:45:03 +0200

    inl-mningsuppgift-2-c

--- main.c
+++ main.c
@@ -2,7 +2,13 @@
 
 
 // This is where you add the two functions
+float fahrenheit_to_celsius(float temp_f){
+    return ((temp_f-32) * 5.0) / (9.0);
+}
 
+float celsius_to_fahrenheit(float temp_c){
+    return ((temp_c * 9.0) / (5.0)) + 32;
+}
 int main() {
     float temp_f = 100.0;
     float temp_c = 37.0;
